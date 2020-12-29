8172c66428e40f2f6e716472312beffebc9c1b22
SohamSiddhid <74717377+SohamSiddhid@users.noreply.github.com>
2020-12-29 11:26:14 +0530

    Add files via upload

---
+++ lmyOwnLibrary.js
@@ -0,0 +1,14 @@
+//function for colliding any 2 objects
+function hasCollide(object1,object2){
+    object1RightEdge = object1.x + object1.width;
+    object2LeftEdge = object2.x;
+ 
+ if(object1RightEdge>=object2LeftEdge){
+    return true;
+ }
+   
+ else{
+ return false;
+ }
+ }
+ 
